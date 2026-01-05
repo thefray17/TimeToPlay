db7012ce7e82b6bc5d322b02d573f6403f80b7d4
Fred Fortuito <ffortuito@gmail.com>
2026-01-05 04:43:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6826177348-21a71",
+  "appId": "1:981104178636:web:2a06e37e9aa753ce6d9969",
+  "apiKey": "AIzaSyCewFez9dWzkhaGARH-Ic5fdHIP1TU972U",
+  "authDomain": "studio-6826177348-21a71.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "981104178636"
+};
